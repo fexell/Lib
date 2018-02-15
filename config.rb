e997871207690276da1322b9ce653c937b86a7c7
Felix Cervin <fexellofficial@gmail.com>
2018-02-15 20:51:19 +0100

    Delete config.rb

--- config.rb
+++
@@ -1,9 +0,0 @@
-
-css_dir = "dist/css" # by Compass.app 
-sass_dir = "scss" # by Compass.app 
-images_dir = "assets/media" # by Compass.app 
-output_style = :expanded # by Compass.app 
-relative_assets = false # by Compass.app 
-line_comments = false # by Compass.app 
-sass_options = {:debug_info=>false} # by Compass.app 
-sourcemap = false # by Compass.app 
