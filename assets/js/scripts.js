e88c0620014958ac287d56d0ba0c2d01c85388d5
Felix Cervin <fexellofficial@gmail.com>
2018-03-01 11:14:36 +0100

    Colors and respective classes added
    Colors and .bg- , .color- added.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,11 @@
-(function(window, document, undefined) {
-    var g = window.document;
-    
+// global "console"
+(function (window, document, undefined) {
+  "use strict";
+  var i = 0,
+      g = window.document;
+  
+  var conlog = function(e) {
+    console.log(e);
+  }
+  
 })(this, this.document);
